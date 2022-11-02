c590ad783c5e34906efbbcc7a0932dfda3a48457
Ben Elliott <ben.elliott@bevalued.co.uk>
2022-11-02 14:49:02 +0000

    Logging out events and context

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const github = require('@actions/github');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    const labelName = core.getInput('label-name');
+
+    core.info('labelName required is: ' + labelName);
+
+    const context = github.context.payload;
+
+    console.log('eventName:' + github.context.eventName);
+
+    console.log(JSON.stringify(github.context));
+    
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
