e713a5688773570d3b57a256ca7b58851c74d823
Ben Elliott <ben.elliott@bevalued.co.uk>
2022-11-02 15:36:34 +0000

    Added logging and tweaked check steps

--- index.js
+++ index.js
@@ -8,10 +8,28 @@ try {
     core.info('labelName required is: ' + labelName);
     
     // Get the JSON webhook payload for the event that triggered the workflow
-    
-    if (github.context.payload.pull_request.labels.every(l=>l!==labelName)) {
+
+    let hasLabel = false;
+
+    //if (github.context.payload.pull_request.labels.every(l=>l!==labelName)) {
+    //    core.setFailed("This PR does not have the label: " + labelName);
+    //}
+
+    core.info('Current labels applied to this PR: ');
+
+    github.context.payload.pull_request.labels.forEach(label => {
+        core.info(label);
+
+        if (label === labelName) {
+            core.info('Labels match!');
+            hasLabel = true;
+        }
+    });
+
+    if (!hasLabel) {
         core.setFailed("This PR does not have the label: " + labelName);
     }
+
 } catch (error) {
     core.setFailed(error.message);
 }
