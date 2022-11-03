bfc5b876a7762731ddf6f1d8aadd34e386141d11
Ben Elliott <ben.elliott@bevalued.co.uk>
2022-11-03 15:01:30 +0000

    Tweaked index file, updated readme and compiled index

--- index.js
+++ index.js
@@ -6,29 +6,13 @@ try {
     const labelName = core.getInput('label-name');
 
     core.info('labelName required is: ' + labelName);
-    
+
     // Get the JSON webhook payload for the event that triggered the workflow
 
-    let hasLabel = false;
-
-    //if (github.context.payload.pull_request.labels.every(l=>l!==labelName)) {
-    //    core.setFailed("This PR does not have the label: " + labelName);
-    //}
-
-    core.info('Current labels applied to this PR: ');
-
-    github.context.payload.pull_request.labels.forEach(label => {
-        core.info(label.name);
-
-        if (label.name === labelName) {
-            core.info('Labels match!');
-            hasLabel = true;
-        }
-    });
-
-    if (!hasLabel) {
+    if (github.context.payload.pull_request.labels.every(l => l.name !== labelName)) {
         core.setFailed("This PR does not have the label: " + labelName);
     }
+
 
 } catch (error) {
     core.setFailed(error.message);
